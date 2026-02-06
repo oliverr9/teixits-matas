285aa5c373020cd5c999e4c3dcc6045390660aba
Jan Bonaterra <janbonaterra@gmail.com>
2026-02-06 12:58:43 +0100

    Menu integrat + footer + animacions

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,3 +1,4 @@
+import animations from '@midudev/tailwind-animations'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -10,5 +11,7 @@ export default {
         },
       },
     },
-  plugins: [],
+  plugins: [
+    animations
+  ],
 }
